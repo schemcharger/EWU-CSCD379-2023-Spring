b6cffa0aa591404e4d82ef911fb678e1e7145c8c
schemcharger <ngraham7@ewu.edu>
2023-04-25 16:01:59 -0700

    add character page

---
+++ Vaesen_Character_Sheet/src/views/CharacterView.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+      <h1>This is a character sheet</h1>
+      <v-list>
+        <v-list-subheader>todo</v-list-subheader>
+        <v-list-item>add character details</v-list-item>
+        <v-list-item>add character portrait</v-list-item>
+        <v-list-item>add character stats</v-list-item>
+        <v-list-item>add character skills</v-list-item>
+        <v-list-item>add character talents</v-list-item>
+        <v-list-item>add character equipment</v-list-item>
+        <v-list-item>add character notes</v-list-item>
+      </v-list>
+    </main>
+  </template>
