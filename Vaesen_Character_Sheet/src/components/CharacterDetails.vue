68f644db290efc26118dd7d2ec99aa535a9bdefd
schemcharger <ngraham7@ewu.edu>
2023-04-27 15:58:59 -0700

    add vuetify

--- Vaesen_Character_Sheet/src/components/CharacterDetails.vue
+++ Vaesen_Character_Sheet/src/components/CharacterDetails.vue
@@ -1,3 +1,4 @@
 <template>
-    
+    these are character details
+    <v-text-field label="name" clearable></v-text-field>
 </template>
--- Vaesen_Character_Sheet/src/views/CharacterView.vue
+++ Vaesen_Character_Sheet/src/views/CharacterView.vue
@@ -1,21 +1,17 @@
 <template>
   <main>
-    <h1>This is a character sheet</h1>
     <CharacterDetails />
-    <v-card>
-        <ul>
-        <li>add details</li>
-        <li>add portrait</li>
-        <li>add attributes</li>
-        <li>add conditions</li>
-        <li>add skills</li>
-        <li>add talents</li>
-        <li>add insights/defects</li>
-        <li>add Advantages</li>
-        <li>add equipment</li>
-        <li>add notes</li>
-        </ul>
-    </v-card>
+    <!--<ul>
+    <li>add portrait</li>
+    <li>add attributes</li>
+    <li>add conditions</li>
+    <li>add skills</li>
+    <li>add talents</li>
+    <li>add insights/defects</li>
+    <li>add Advantages</li>
+    <li>add equipment</li>
+    <li>add notes</li>
+    </ul>-->
   </main>
 </template>
 
